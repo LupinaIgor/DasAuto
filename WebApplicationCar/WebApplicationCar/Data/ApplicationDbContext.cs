5f8e6e1e0a1124b646104eea10a561297d169a4f
LupinaIgor <igorlupina222@gmail.com>
2024-08-29 11:54:50 +0300

    Create Colors

---
+++ WebApplicationCar/WebApplicationCar/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationCar.Models.Cars.Toyota;
+
+namespace WebApplicationCar.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public DbSet<ColorModel> Colors { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ WebApplicationCar/WebApplicationCar/Models/Cars/Toyota/ColorModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplicationCar.Models.Cars.Toyota;
+
+public class ColorModel
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public string Url { get; set; }
+    
+    public string RGB { get; set; }
+    
+    public string Code { get; set; }
+}
